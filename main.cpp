91897779fe9c8dfefba53c7ee7ec3284fa43cb97
Eduardo Romagnoli <mario@test>
2019-05-31 09:38:22 +0200

    versione 2 master

--- main.cpp
+++ main.cpp
@@ -3,10 +3,9 @@ void main()
 	int contatore, i2=0;
 	for(int i=0;i<10;i++)
 	{
-		contatore = i+10;
+		contatore = i+1;
 		cout <<contatore<<endl;
-		contatore = i2+1;
-		cout <<contatore<<endl;
+		cut <<"abcdsdhsiuadhuishas"<<endl;
 		i2++;
 	}
 }
