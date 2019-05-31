616db2a3b8c092d9313770bd359744e5c4cfa293
Eduardo Romagnoli <mario@test>
2019-05-31 09:36:42 +0200

    versione 1 FIX_BUG

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 void main()
 {
-	int contatore;
+	int contatore, i2=0;
 	for(int i=0;i<10;i++)
 	{
-		contatore = i+1;
+		contatore = i+10;
 		cout <<contatore<<endl;
+		contatore = i2+1;
+		cout <<contatore<<endl;
+		i2++;
 	}
 }
